269acfaa9fd7994719b9c4f4c5df8acd2cfa8f66
Abhiabhishek9 <abhishekkumar.uemk.cse@gmail.com>
2017-06-15 05:44:34 -0700

    Add files via upload
    JAVAProgram

---
+++ add.java
@@ -0,0 +1,15 @@
+class add {
+	  public static void main(String args[])
+	{
+		int a,b,c,scanner;
+		
+		System.out.println("Enter the two numbers to add:");	
+	Scanner in = new
+Scanner(System.in);
+		a = in.nextInt();
+		b = in.nextInt();
+		c = a+b;
+		System.out.println("\nSum of the numbers:"+c);
+	}	
+	
+}
